d0e2fe25de714ade3c3ca619146ef4cf869e84e1
t-mario-y <t.mario.yoshi@gmail.com>
2023-03-25 15:40:40 +0900

    enable split keyboard in example

--- keymap/examples/keymap.rb
+++ keymap/examples/keymap.rb
@@ -1,13 +1,16 @@
 kbd = Keyboard.new
 
+kbd.split = true
+kbd.uart_pin = 0
+
 kbd.init_pins(
   [14, 15], # rows
   [17, 16] # columns
 )
 
 kbd.add_layer :default, %i(
-  KC_Q  KC_W
-  KC_A  KC_S
+  KC_Q  KC_W  KC_E  KC_R
+  KC_A  KC_S  KC_D  KC_F
 )
 
 kbd.start!
